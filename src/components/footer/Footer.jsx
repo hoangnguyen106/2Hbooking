78cb84d9dadf4f7aebbbb68e6ef036f89b7ec8b7
hoang nguyen <hoangden1062000@gmail.com>
2022-06-09 13:10:12 +0700

    2Hbooking

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,61 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fLists">
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regons</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districs</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+            </div>
+            <div className="fText">Coppyright © 2022 2Hbooking</div>
+        </div>
+    )
+};
+
+export default Footer;
